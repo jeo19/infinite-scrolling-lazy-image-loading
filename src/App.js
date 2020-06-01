e35cd6a284554e9e1f4261de054a680fe00fc505
jeo19 <terrakkk@outlook.com>
2020-06-02 01:21:05 +0800

    add:Apply the Bootstrap's CSS link in the 'public/index.html' and load the picture with using fetch API

--- src/App.js
+++ src/App.js
@@ -13,9 +13,57 @@ function App() {
         }
     };
     const [imgData, imgDisPatch] = useReducer(imgReducer, {
-        image: [],
+        images: [],
         fetching: true,
     });
+    useEffect(() => {
+        imgDisPatch({ type: 'FETCHING_IMAGES', fetching: true });
+        fetch('https://picsum.photos/v2/list?page=0&limit=10')
+            .then((data) => data.json())
+            .then((images) => {
+                imgDisPatch({ type: 'STACK_IMAGES', images });
+                imgDisPatch({ type: 'FETCHING_IMAGES', fetching: false });
+            })
+            .catch((e) => {
+                // handle error
+                imgDisPatch({ type: 'FETCHING_IMAGES', fetching: false });
+                return e;
+            });
+    }, [imgDisPatch]);
+    return (
+        <div className="">
+            <nav className="navbar bg-light">
+                <div className="container">
+                    <a className="navbar-brand" href="/#">
+                        <h2>Infinite scroll + image lazy loading</h2>
+                    </a>
+                </div>
+            </nav>
+            <div id="images" className="container">
+                <div className="row">
+                    {imgData.images.map((image, index) => {
+                        const { author, download_url } = image;
+                        return (
+                            <div key={index} className="card">
+                                <div className="card-body ">
+                                    <img
+                                        alt={author}
+                                        className="card-img-top"
+                                        src={download_url}
+                                    />
+                                </div>
+                                <div className="card-footer">
+                                    <p className="card-text text-center text-capitalize text-primary">
+                                        Shot by: {author}
+                                    </p>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
