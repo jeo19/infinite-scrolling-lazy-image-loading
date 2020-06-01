ea10a937b97dea237e285c59c6a45012ad1ba58a
jeo19 <terrakkk@outlook.com>
2020-06-01 23:47:58 +0800

    add:Add useReducer Hook for image lazy loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useReducer } from 'react';
+import './index.css';
 
 function App() {
-  return <div className="App">Hello</div>;
+    const imgReducer = (state, action) => {
+        switch (action.type) {
+            case 'STACK_IMAGES':
+                return { ...state, images: state.images.concat(action.images) };
+            case 'FETCHING_IMAGES':
+                return { ...state, fetching: action.fetching };
+            default:
+                return state;
+        }
+    };
+    const [imgData, imgDisPatch] = useReducer(imgReducer, {
+        image: [],
+        fetching: true,
+    });
 }
 
 export default App;
